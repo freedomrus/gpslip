54832fc0bdaab96c2ac3ae449b3996eafd1be765
Dmitriy_Kovalenko <imfreedomyt@gmail.com>
2023-03-31 19:46:37 +0300

    fefe

--- src/components/ApplicationPopUp.vue
+++ src/components/ApplicationPopUp.vue
@@ -14,44 +14,65 @@
 					<span class="title-form">Заполните форму</span>
 					<div class="group">
 						<input
-							required
+
 							class="textinput"
 							type="text"
-							v-model="fullName"
+							:class="$v.fullName.$error ? 'is-invalid' : ''"
+							v-model.trim="fullName"
 							placeholder="Иванов Иван Иванович"
 						>
 						<span class="bar"></span>
 						<label>ФИО</label>
+						<p v-if="$v.fullName.$dirty && !$v.fullName.required" class="invalid-feedback">
+							Обязательное поле
+						</p>
 					</div>
 
 					<div class="group">
 						<input
-							required
+
 							class="textinput"
 							type="text"
 							v-model.trim="phone"
 							v-imask="phoneNumberMask"
 							maxlength="16"
+							:class="$v.phone.$error ? 'is-invalid' : ''"
 							@keypress="isNumber"
 							placeholder="+7(912)312-31-23"
 
 						>
 						<span class="bar"></span>
 						<label>Контактный телефон</label>
+						<p v-if="$v.phone.$dirty && !$v.phone.required" class="invalid-feedback">
+							Обязательное поле
+						</p>
+						<p v-if="$v.phone.$dirty && !$v.phone.minLength" class="invalid-feedback">
+							Введите все цифры телефона
+						</p>
 					</div>
 
 					<div class="group">
 						<input
-							required
+
 							class="emailinput"
-							type="text"
-							v-model="email"
+							type="email"
+							:class="$v.email.$error ? 'is-invalid' : ''"
+							v-model.trim="email"
 							placeholder="ztmlipetsk@gmail.com"
 						>
 						<span class="bar"></span>
 						<label>Email</label>
+						<p v-if="$v.email.$dirty && !$v.email.required" class="invalid-feedback">
+							Обязательное поле
+						</p>
+						<p class="invalid-feedback">{{errorMessage}}</p>
 					</div>
-
+					<div class="group">
+						<div class="wishes-title">Ваши пожелания</div>
+						<div class="wishes">
+							<textarea v-model="wishes" cols="30" rows="6" maxlength="1000"></textarea>
+						</div>
+					</div>
 					<button class="sendbtn" type="submit">Отправить</button>
 				</div>
 			</form>
@@ -75,8 +96,11 @@ export default {
 			fullName: "",
 			phone: "",
 			email: "",
+			wishes: "",
+			errorMessage: '',
 			// bot config
 			token: "6272564398:AAGU7TVTEZqw9JiXelsimOqIOx0ooqCtDuw",
+			// chatId: '',
 			chatId: -702085161,
 			phoneNumberMask: {
 				mask: '+{0}(000)000-00-00',
@@ -96,39 +120,52 @@ export default {
 
 	methods: {
 
+		// validateEmail(email) {
+		// 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+		// 		this.errorMessage = ''
+		// 	} else {
+		// 		this.errorMessage = 'Введите email по образцу'
+		// 	}
+		// },
+
 		sendEmail() {
-			const SERVICE_ID = 'service_gpslip'; // ID вашего сервиса emailjs
-			const TEMPLATE_ID = 'template_gpslip'; // ID вашего шаблона emailjs
-			const USER_ID = 'YMXZ3pgLifxND2caD'; // ID вашего пользователя emailjs
+			this.$v.$touch()
+			if (!this.$v.$error) {
 
-			const params = {
-				from_name: this.fullName,
-				from_email: this.email,
-				from_phone: this.phone,
-				to_name: 'GpsLip', // Имя получателя
-			};
 
-			let fullMessage = `Заявка\nФИО: ${this.fullName}\nТелефон: ${this.phone}\nПочта: ${this.email}`
-			this.$http.post(`https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatId}&text=${fullMessage}`)
-				.then(response => {
-					console.log("Успешно", response)
-				}, error => {
-					console.log(error)
-				})
+				const SERVICE_ID = 'service_gpslip'; // ID вашего сервиса emailjs
+				const TEMPLATE_ID = 'template_gpslip'; // ID вашего шаблона emailjs
+				const USER_ID = 'YMXZ3pgLifxND2caD'; // ID вашего пользователя emailjs
 
-			emailjs.send(SERVICE_ID, TEMPLATE_ID, params, USER_ID)
-				.then(() => {
-					console.log('Email sent successfully!');
-					this.phone = '';
-					this.fullName = '';
-					this.email = '';
-					this.closeapplication()
-				}, error => {
-					console.error('Email failed to send:', error);
-				});
+				const params = {
+					from_name: this.fullName,
+					from_email: this.email,
+					from_phone: this.phone,
+					from_wishes: this.wishes,
+					to_name: 'GpsLip', // Имя получателя
+				};
+
+				let fullMessage = `Заявка от клиента\nФИО: ${this.fullName}\nКонтактный телефон: ${this.phone}\nПочта: ${this.email}\nПожелания: "${this.wishes}"`
+				this.$http.post(`https://api.telegram.org/bot${this.token}/sendMessage?chat_id=${this.chatId}&text=${fullMessage}`)
+					.then(response => {
+						console.log("Успешно", response)
+					}, error => {
+						console.log(error)
+					})
+
+				emailjs.send(SERVICE_ID, TEMPLATE_ID, params, USER_ID)
+					.then(() => {
+						console.log('Email sent successfully!');
+						this.phone = '';
+						this.fullName = '';
+						this.email = '';
+						this.wishes = '';
+						this.closeapplication()
+					}, error => {
+						console.error('Email failed to send:', error);
+					});
+			}
 		},
-
-
 
 
 		isNumber(e) {
@@ -161,6 +198,12 @@ export default {
 	directives: {
 		imask: IMaskDirective
 	},
+	// watch: {
+	// 	email(value){
+	// 		this.email = value;
+	// 		this.validateEmail(value);
+	// 	}
+	// },
 }
 </script>
 
@@ -171,13 +214,15 @@ export default {
 
 // Form
 
-.is-invalid{
-	border: 1px solid red;
+.is-invalid {
+	box-shadow: 2px 2px 6px 2px rgba(224, 28, 34, 0.5);
 }
-.invalid-feedback{
-	color: #F3CD00;
+
+.invalid-feedback {
+	color: red;
 	margin-top: size(10, 1920);
-	font-size: size(20, 1920);
+	font-size: size(24, 1920);
+	font-weight: 600;
 }
 
 
@@ -210,6 +255,28 @@ form {
 		margin: size(30, 1920) 0 size(10, 1920) 0;
 	}
 
+	.wishes-title {
+		font-family: 'Montserrat', sans-serif;
+		font-weight: 700;
+		font-size: size(25, 1920);
+		line-height: size(26, 1920);
+		color: #FFFFFF;
+		margin-bottom: size(10, 1920);
+
+	}
+
+	textarea {
+		font-size: size(22, 1920);
+		padding: size(20, 1920);
+		width: size(460, 1920);
+		height: size(150, 1920);
+		color: #075985;
+		background: rgba(255, 255, 255, 0.8);
+		box-shadow: 2px 2px 6px 2px rgba(7, 89, 133, 0.5);
+		border-radius: 10px;
+		resize: none;
+	}
+
 	input {
 		font-size: size(22, 1920);
 		padding: size(10, 1920) size(10, 1920) size(10, 1920) size(5, 1920);
@@ -229,29 +296,29 @@ form {
 
 	/* LABEL ======================================= */
 	label {
-		color: #075985;
-		font-size: size(18, 1920);
 		font-weight: normal;
 		position: absolute;
 		pointer-events: none;
 		left: size(8, 1920);
-		top: size(14, 1920);
 		transition: 0.4s all;
+		top: size(-20, 1920);
+		font-size: size(16, 1920);
+		color: white;
 	}
 
 	/* active state */
 
-	.emailinput:focus ~ label, .emailinput:valid ~ label {
-		top: size(-20, 1920);
-		font-size: size(16, 1920);
-		color: white;
-	}
-
-	.textinput:focus ~ label, .textinput:valid ~ label {
-		top: size(-20, 1920);
-		font-size: size(16, 1920);
-		color: white;
-	}
+	//.emailinput:focus ~ label, .emailinput:valid ~ label {
+	//	top: size(-20, 1920);
+	//	font-size: size(16, 1920);
+	//	color: white;
+	//}
+	//
+	//.textinput:focus ~ label, .textinput:valid ~ label {
+	//	top: size(-20, 1920);
+	//	font-size: size(16, 1920);
+	//	color: white;
+	//}
 
 
 	::placeholder {
@@ -287,18 +354,20 @@ form {
 
 	.bar:before {
 		left: 50%;
+
 	}
 
 	.bar:after {
 		right: 50%;
+
 	}
 
 	/* active state */
-	.emailinput:valid ~ .bar:before, .emailinput:valid ~ .bar:after, {
+	.emailinput:invalid ~ .bar:before, .emailinput:invalid ~ .bar:after, {
 		width: 50%;
 	}
 
-	.textinput:valid ~ .bar:before, .textinput:valid ~ .bar:after, {
+	.textinput:invalid ~ .bar:before, .textinput:invalid ~ .bar:after, {
 		width: 50%;
 	}
 
@@ -402,7 +471,7 @@ form {
 	align-items: center;
 	flex-direction: column;
 	width: size(800, 1920);
-	height: 60%;
+	height: 95%;
 	background: linear-gradient(126.76deg, rgba(6, 123, 205, 0.4) 0%, rgba(224, 28, 34, 0.4) 100%);
 	//background-color: rgba(255, 255, 255, 0.7);
 	padding: size(40, 1920);
@@ -451,7 +520,7 @@ form {
 }
 
 .modal-content::-webkit-scrollbar-track {
-	-webkit-box-shadow: inset 0 0 6px rgba(6,123,205,0.3) !important;
+	-webkit-box-shadow: inset 0 0 6px rgba(6, 123, 205, 0.3) !important;
 }
 
 </style>
